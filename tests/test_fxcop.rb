a0c48a62ea43ee50f7373d56a34ced458b2c720a
Iain Mitchell <iainjmitchell@gmail.com>
2013-04-24 16:13:45 +0100

    ''

--- tests/test_fxcop.rb
+++ tests/test_fxcop.rb
@@ -3,52 +3,60 @@ require 'test/unit'
 class TestFxCop < Test::Unit::TestCase
 	def test_when_started_with_no_arguments_Then_command_is_fxcop_binary	
 		fxcop_binary = '.\FxCopCmd.exe'
-		FxCop.new(self, fxcop_binary: fxcop_binary).start	
+		BuildQuality::FxCop.new(self, fxcop_binary: fxcop_binary).start	
 		assert_equal(fxcop_binary, @command)
 	end
 
-	def test_when_started_with_assembly_argument_Then_command_is_file_switch_with_assembly	
-		assembly = 'c:\workspace\mydll.dll'
+	def test_when_fxcop_started_with_one_assembly_Then_command_contains_file_switched_assembly
+		assembly = 'c:\workspace\mydll1.dll'
 		file_switch = '/f:'		
-		FxCop.new(self,{}).start(assembly: assembly)	
-		assert_equal("#{file_switch}#{assembly}", @command)
+		BuildQuality::FxCop.new(self, {}).start(assemblies: [assembly])
+		assert_equal(" #{file_switch}#{assembly}", @command)
 	end
+	
 
-	# def test_when_started_with_binary_and_assembly_arguments_Then_command_contains_both
-	# 	assert_equal()
-	# end
 
 	def execute(command)
 		@command = command
 	end
 end
 
-class FxCop
-	def initialize(shell_command, parameters)	
-		fxcop_binary = parameters[:fxcop_binary]
-		@fxcop_command = FxCopCommand.new(fxcop_binary,shell_command)
+module BuildQuality
+	class FxCop
+		def initialize(shell_command = ShellCommand.new, parameters)	
+			fxcop_binary = parameters[:fxcop_binary]
+			@fxcop_command = FxCopCommand.new(fxcop_binary,shell_command)
+		end
+
+		def start(parameters = {})						
+			file_command = FileCommandBuilder.build(parameters[:assemblies])
+			@fxcop_command.execute(file_command)
+		end
 	end
 
-	def start(parameters = {})
-		file_command = FileCommandBuilder.build(parameters[:assembly])
-		@fxcop_command.execute(file_command)
-	end
-end
+	class FxCopCommand
+		def initialize(fxcop_binary,shell_command)
+			@fxcop_binary = fxcop_binary
+			@shell_command = shell_command
+		end
 
-class FxCopCommand
-	def initialize(fxcop_binary,shell_command)
-		@fxcop_binary = fxcop_binary
-		@shell_command = shell_command
+		def execute(file_command)
+			fxcop_command = "#{@fxcop_binary}#{file_command}"
+			@shell_command.execute(fxcop_command)
+		end
 	end
 
-	def execute(file_command)
-		fxcop_command = "#{@fxcop_binary}#{file_command}"
-		@shell_command.execute(fxcop_command)
-	end
-end
+	class FileCommandBuilder
+		FILE_SWITCH = "/f:"
 
-class FileCommandBuilder
-	def self.build(assembly)
-		"/f:#{assembly}" unless assembly.nil?
+		def self.build(assemblies)
+			" #{FILE_SWITCH}#{assemblies[0]}" unless assemblies.nil?
+		end
+	end
+
+	class ShellCommand
+		def execute(command)
+			system command	
+		end
 	end
 end
