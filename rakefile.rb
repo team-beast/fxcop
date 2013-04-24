3de3c8985306992bc5ee46f5bc366e6bf94a003c
Iain Mitchell <iainjmitchell@gmail.com>
2013-04-24 13:45:22 +0100

    'adding_rakefile_and_automatic_push_to_git'

---
+++ rakefile.rb
@@ -0,0 +1,27 @@
+task :default => [:dependencies, :unit_tests, :commit]
+
+desc 'Load dependencies with bundler'
+task :dependencies do
+	system "bundle update"
+end
+
+desc 'Run unit tests'
+task :unit_tests do
+	require 'peach'
+	TEST_FILE_PATTERN = 'tests/**/*.rb'
+	Dir[TEST_FILE_PATTERN].peach do | test_file_name |
+		puts ">> Running tests on: #{test_file_name}"
+		system "ruby #{test_file_name}"
+	end
+end
+
+desc 'Committing and Pushing to Git'
+task :commit do	
+	require 'git_repository'
+	commit_message = "#{ENV["m"]}" || 'no commit message'
+	p commit_message
+	git = GitRepository.new
+	git.add
+	git.commit(:message => commit_message)
+	git.push	
+end
